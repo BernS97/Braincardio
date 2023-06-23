2da0b112ef4ddce139918eee36754ccb4c768b4e
mseitz <moritzseitz97@gmail.com>
2023-06-23 19:41:29 +0200

    fix main add js-confetti

--- src/components/Duel/DuelDoneCard.vue
+++ src/components/Duel/DuelDoneCard.vue
@@ -1,6 +1,6 @@
 <template>
     <ion-content :scroll-y="false" :fullscreen="true" v-if="duel?.done">
-        <ion-card v-if="winner?.user" class="winner">
+        <ion-card v-if="winner?.user" class="winner" @click="addConfetti">
             <user-avatar :userProfile="winner?.user" />
             <ion-card-header>
                 <ion-card-title>{{ winner?.user?.name }}</ion-card-title>
@@ -28,19 +28,21 @@
                 {{ $t('drawText') }}
             </ion-card-content>
         </ion-card>
+
+
     </ion-content>
 </template>
 <script setup>
 import { IonContent, IonCard, IonCardContent, IonCardTitle, IonCardSubtitle, IonCardHeader, IonRow, IonCol } from '@ionic/vue';
 import UserAvatar from "@/components/Base/UserAvatar.vue";
-import { useDocument } from "vuefire";
-import { db } from '@/plugins/firebase';
-import { ref } from "vue";
-import { doc } from 'firebase/firestore';
+import { onMounted, ref } from "vue";
+import JSConfetti from 'js-confetti'
+
+const jsConfetti = new JSConfetti();
+
 const props = defineProps(["duel"]);
 const winner = ref(null);
-props.duel.users.forEach(userPath => {
-    const user = useDocument(doc(db, userPath));
+props.duel.users.forEach(user => {
     const corrects = props.duel.turns.filter((turn) => turn.userId == user.id && turn.result == 1).length;
     if (!winner.value || winner.value.corrects < winner?.value?.corrects)
         winner.value = {
@@ -48,6 +50,19 @@ props.duel.users.forEach(userPath => {
             user: user,
         }
 });
+
+onMounted(() => {
+    addConfetti();
+    addConfetti();
+    addConfetti();
+})
+
+const addConfetti = () => {
+    jsConfetti.addConfetti(
+        {
+            emojis: [winner.value.user.image.emoji]
+        });
+}
 </script>
 <style>
 .winner .userAvatarBackground {
