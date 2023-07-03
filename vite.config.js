476f325adca22fb6abadfc6ac16066dee2c99afe
mseitz <moritzseitz97@gmail.com>
2023-07-03 21:40:28 +0200

    minify build

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,10 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), legacy()],
+  build: {
+    minify: true,
+    cssMinify: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
